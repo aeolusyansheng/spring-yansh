1b88c018de88c61d9a792f01106f48b9ccdbb03b
z2h2980 <sho.gen.907@ctc-g.co.jp>
2018-01-19 18:32:29 +0900

    2018/02/14 windows-1

--- src/com/yansheng/beans/PropertyAccessor.java
+++ src/com/yansheng/beans/PropertyAccessor.java
@@ -27,14 +27,14 @@ public interface PropertyAccessor {
 
 	void setPropertyValue(String propertyName, Object value) throws BeansException;
 
-	void setPropertValue(PropertyValue pv) throws BeansException;
+	void setPropertyValue(PropertyValue pv) throws BeansException;
 
-	void setPropertyValue(Map<?, ?> map) throws BeansException;
+	void setPropertyValues(Map<?, ?> map) throws BeansException;
 
-	void setPropertyValue(PropertyValues pvs) throws BeansException;
+	void setPropertyValues(PropertyValues pvs) throws BeansException;
 
-	void setPropertyValue(PropertyValues pvs, boolean ignoreUnknown) throws BeansException;
+	void setPropertyValues(PropertyValues pvs, boolean ignoreUnknown) throws BeansException;
 
-	void setPropertyValue(PropertyValues pvs, boolean ignoreUnknown, boolean ignoreInvalid) throws BeansException;
+	void setPropertyValues(PropertyValues pvs, boolean ignoreUnknown, boolean ignoreInvalid) throws BeansException;
 
 }
