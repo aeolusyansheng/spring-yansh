7b7df03d215ca83bba70b9f6ca07a3f0af24797b
z2h2980 <sho.gen.907@ctc-g.co.jp>
2018-04-02 17:13:54 +0900

    2018/04/02 windows-2

--- doc/25.データベース設計/01.論理設計/DDL/update_ddl/update_ddl_20130823.sql
+++
@@ -1,6 +0,0 @@
--- 2013/8/23 インデックス追加
--- 稼働入力履歴トラン [ 入力者ID ]、[ 稼働日 ]、[ 稼働ステータス ]に複合インデックスを付与
-CREATE INDEX IX1_PJTR_MI_OPENT_HIST ON PJTR_MI_OPENT_HIST (
-    ENT_PSN_ID, OPE_DT, OPE_STATS
-);
-COMMENT ON TABLE PJTR_MI_OPENT_HIST IS '稼働入力履歴トラン(ver.0.8)';
